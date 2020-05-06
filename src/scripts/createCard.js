c2e3b7664805228b7d495b34f66961b4cf8898f5
Thiele <antoniocarloss60@gmail.com>
2020-05-06 14:55:18 -0300

    listando os quartos

--- src/scripts/createCard.js
+++ src/scripts/createCard.js
@@ -21,22 +21,65 @@ getData('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72', {})
     cardsElement.innerHTML = '';
     
         for (apartment of apartments) {
-            var h1Element = document.createElement('h1');
-            var h1Text = document.createTextNode(apartment.name);
+            var cardElement = document.createElement('div');
+            cardElement.setAttribute('class', 'card');
+
+            var cardHorizontalElement = document.createElement('div');
+            cardHorizontalElement.setAttribute('class', 'card-horizontal');
+
+            var imgSquareWrapperElement = document.createElement('div');
+            imgSquareWrapperElement.setAttribute('class', 'img-square-wrapper');
+
+            var imgElement = document.createElement('img');
+            imgElement.setAttribute('src', apartment.photo);
+            imgElement.setAttribute('class', 'img-fluid');
+
+            imgSquareWrapperElement.appendChild(imgElement);
+
+            var bodyElement = document.createElement('div');
+            bodyElement.setAttribute('class', 'card-body');
+
+            var cardTitleElement = document.createElement('h5');
+            cardTitleElement.setAttribute('class', 'card-title');
+            var cardTitleElementText = document.createTextNode(apartment.name);
+            cardTitleElement.appendChild(cardTitleElementText);
+
+            var propertyTypeElement = document.createElement('p');
+            propertyTypeElement.setAttribute('class', 'card-text');
+            var propertyTypeElementText = document.createTextNode('Tipo de propriedade: ' + apartment.property_type);
+            propertyTypeElement.appendChild(propertyTypeElementText);
+
+            var priceElement = document.createElement('p');
+            priceElement.setAttribute('class', 'card-text');
+            var priceElementElementText = document.createTextNode('Preço da diária: $' + apartment.price);
+            priceElement.appendChild(priceElementElementText);
+
+            bodyElement.appendChild(cardTitleElement);
+            bodyElement.appendChild(propertyTypeElement);
+            bodyElement.appendChild(priceElement);
     
-            //var linkElement = document.createElement('a');
+            //Apends
+
+            cardHorizontalElement.appendChild(imgSquareWrapperElement);
+            cardHorizontalElement.appendChild(bodyElement);
+
+            cardElement.appendChild(cardHorizontalElement);
     
-            //linkElement.setAttribute('href', '#');
     
-            //var pos = todos.indexOf(todo);
-            //linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
-    
-            //var linkText = document.createTextNode('Excluir');
-    
-            //linkElement.appendChild(linkText);
-    
-            h1Element.appendChild(h1Text);
-    
-            cardsElement.appendChild(h1Element);
+            cardsElement.appendChild(cardElement);
         }
     }
+
+    /**
+     <div class="card">*
+            <div class="card-horizontal">*
+
+              <div class="img-square-wrapper">
+              </div>
+
+              <div class="card-body">
+              </div>
+
+            </div>
+          </div>
+     */
